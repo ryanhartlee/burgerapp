c00ddc4dbc1e41639d3f0cf521132972c1dc8aa1
Ryan Hartley <ryanhartlee@gmail.com>
2019-02-22 14:05:22 -0700

    mostly done

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insert([
+        "burger_name", 
+    ], [
+            req.body.burger_name, 
+        ], function (result) {
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDonalds', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wendys', false);
